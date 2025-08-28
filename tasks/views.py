649a71028ad0a9090e081272ecd7c44a001316c2
Gerardo-Rioss <gerarios26@gmail.com>
2025-08-28 12:09:12 -0300

    Initial commit

---
+++ tasks/views.py
@@ -0,0 +1,8 @@
+# Create your views here.
+from rest_framework import viewsets
+from .models import Task
+from .serializer import TaskSerializer
+
+class TaskView(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer

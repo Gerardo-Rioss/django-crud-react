180760b93301eb417c3290ad307bd68ef9e6f4a4
Gerardo-Rioss <gerarios26@gmail.com>
2025-08-29 08:04:50 -0300

    agregado el modulo de la documentacion

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -128,3 +128,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = []
+
+REST_FRAMEWORK = {
+    ...: ...,
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
+}

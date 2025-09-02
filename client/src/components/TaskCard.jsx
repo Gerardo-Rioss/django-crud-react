1b038a9d9e153cd22233ee49b24595e0abdb8a73
Gerardo-Rioss <gerarios26@gmail.com>
2025-09-02 12:16:38 -0300

    agregado delete

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,8 +1,11 @@
+import { useNavigate } from "react-router-dom";
 export default function TaskCard({task}) {
+    const navigate = useNavigate();
   return (
-    <div>
+    <div style={{background: "#101010"}} onClick={()=> navigate(`/tasks/${task.id}`)}>
         <h1>{task.title}</h1>
         <p>{task.description}</p>
+        <hr />
     </div>
   )
 }
